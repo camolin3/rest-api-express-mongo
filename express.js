323ba19f711c5e80e70812906b4994eda7d45d8a
Carlos Molina A <ing.cmolina@gmail.com>
2014-10-06 22:00:18 -0300

    Now it sends tweets to @tweetsaster for every POST to collections/tweets/ Enabled CORS Fixed the responses of the GETs and POST: now it returns the same JSON but inside a wrapper expected by Ember Data Using 'pluralize' and 'twit' npm packages

--- express.js
+++ express.js
@@ -1,54 +1,93 @@
 var express = require('express'),
-  mongoskin = require('mongoskin'),
-  bodyParser = require('body-parser')
+    mongoskin = require('mongoskin'),
+    bodyParser = require('body-parser'),
+    pluralize = require('pluralize'),
+    Twit = require('twit');
 
-var app = express()
-app.use(bodyParser())
+var app = express();
+app.use(bodyParser());
 
-var db = mongoskin.db('mongodb://dsas3_cl:sro1pwRjGxS1@localhost:27017/streamsaster_development', {safe:true})
+var db = mongoskin.db(
+  'mongodb://dsas3_cl:sro1pwRjGxS1@localhost:27017/streamsaster_development', 
+  {safe:true}
+);
+
+var t = new Twit({
+  consumer_key: 'qIxLeqAVHDevyS6bdUfyFmSVg',
+  consumer_secret: 'ZLzqzjGfJayCsQStibiUHdrB2wtRZEFJfjPDkJKVsB7ZnzODYA',
+  access_token: '2559626324-eF4FxEnSF5zj02NeUFifYui8ECMoRiAVgkxSe63',
+  access_token_secret: '4A8J0bUwWcsPWktQ8GkF7SGxzgu3t8MXuV4YnOIoldjw3'
+});
 
 app.param('collectionName', function(req, res, next, collectionName) {
-  req.collection = db.collection(collectionName)
-  return next()
-})
+  req.collection = db.collection(collectionName);
+  return next();
+});
+
+app.all('*', function(req, res, next) { 
+  res.header("Access-Control-Allow-Origin", "*"); 
+  res.header("Access-Control-Allow-Headers", "X-Requested-With"); 
+  next(); 
+});
 
 app.get('/', function(req, res, next) {
-  res.send('please select a collection, e.g., /collections/messages')
-})
+  res.send('please select a collection, e.g., /collections/messages');
+});
 
 app.get('/collections/:collectionName', function(req, res, next) {
-  req.collection.find({} ,{limit: 10, sort: {'_id': -1}}).toArray(function(e, results) {
-    if (e) return next(e)
-    res.send(results)
-  })
-})
+  req.collection.find({}, {limit: 10, sort: {'_id': -1}}).toArray(function(e, results) {
+    if (e) return next(e);
+    var json = {},
+        wrapperName = req.params.collectionName;
+    json[wrapperName] = results;
+    res.send(json);
+  });
+});
 
 app.post('/collections/:collectionName', function(req, res, next) {
-  req.collection.insert(req.body, {}, function(e, results) {
-    if (e) return next(e)
-    res.send(results)
-  })
-})
+  var coordinates = req.body.tweet.geo.coordinates;
+  var newTweet = {
+    status: req.body.tweet.text, 
+    lat: coordinates[0], 
+    long: coordinates[1],
+    display_coordinates: true
+  };
+  t.post('statuses/update', newTweet, function(error, data, response) {
+    if (error) console.log(error);
+    // If succefully posted on Twitter, then save it on the db
+    req.collection.insert(data, {}, function(e, result) {
+      if (e) return next(e);
+      var json = {},
+          wrapperName = pluralize(req.params.collectionName, 1);
+      json[wrapperName] = result;
+      res.send(json);
+    });
+  });
+});
 
 app.get('/collections/:collectionName/:id', function(req, res, next) {
   req.collection.findById(req.params.id, function(e, result) {
-    if (e) return next(e)
-    res.send(result)
-  })
-})
+    if (e) return next(e);
+    var json = {},
+        wrapperName = pluralize(req.params.collectionName, 1);
+    json[wrapperName] = result;
+    res.send(json);
+  });
+});
 
 app.put('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set: req.body}, {safe: true, multi: false}, function(e, result) {
-    if (e) return next(e)
-    res.send((result === 1) ? {msg:'success'} : {msg: 'error'})
-  })
-})
+  req.collection.updateById(req.params.id, {$set: req.body}, 
+                            {safe: true, multi: false}, function(e, result) {
+    if (e) return next(e);
+    res.send((result === 1) ? {msg:'success'} : {msg: 'error'});
+  });
+});
 
 app.delete('/collections/:collectionName/:id', function(req, res, next) {
   req.collection.removeById(req.params.id, function(e, result) {
-    if (e) return next(e)
-    res.send((result === 1)?{msg: 'success'} : {msg: 'error'})
-  })
-})
+    if (e) return next(e);
+    res.send((result === 1) ? {msg: 'success'} : {msg: 'error'});
+  });
+});
 
-app.listen(28017)
+app.listen(28017);
