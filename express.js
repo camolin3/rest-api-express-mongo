91df69cb6397548b3c46ebebc5d8110c2a160b68
Carlos Molina A <ing.cmolina@gmail.com>
2014-11-02 23:38:26 -0300

    Using 'tweets' instead of using a 'collection' param Updating package.json

--- express.js
+++ express.js
@@ -1,7 +1,6 @@
 var express = require('express'),
     mongoskin = require('mongoskin'),
     bodyParser = require('body-parser'),
-    pluralize = require('pluralize'),
     Twit = require('twit');
 
 var app = express();
@@ -19,33 +18,28 @@ var t = new Twit({
   access_token_secret: '4A8J0bUwWcsPWktQ8GkF7SGxzgu3t8MXuV4YnOIoldjw3'
 });
 
-app.param('collectionName', function(req, res, next, collectionName) {
-  req.collection = db.collection(collectionName);
-  return next();
-});
-
 app.all('*', function(req, res, next) { 
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); 
-  next(); 
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (!req.tweets)
+    req.tweets = db.collection('tweets');
+  next();
 });
 
 app.get('/', function(req, res, next) {
   res.send('please select a collection, e.g., /tweets');
 });
 
-app.get('/:collectionName', function(req, res, next) {
-  req.collection.find({in_reply_to_status_id: null}, 
+app.get('/tweets', function(req, res, next) {
+  req.tweets.find({in_reply_to_status_id: null}, 
     {limit: 30, sort: {'_id': -1}}).toArray(function(e, results) {
     if (e) return next(e);
-    var json = {},
-        wrapperName = req.params.collectionName;
-    json[wrapperName] = results;
+    var json = {tweets: results};
     res.send(json);
   });
 });
 
-app.post('/:collectionName', function(req, res, next) {
+app.post('/tweets', function(req, res, next) {
   var params = req.body.tweet, 
       coordinates = params.coordinates.coordinates;
   var newTweet = {
@@ -58,62 +52,54 @@ app.post('/:collectionName', function(req, res, next) {
   t.post('statuses/update', newTweet, function(error, data, response) {
     if (error) console.log(error);
     // If succefully posted on Twitter, then save it on the db
-    req.collection.insert(data, {}, function(e, result) {
+    req.tweets.insert(data, {}, function(e, result) {
       if (e) return next(e);
-      var json = {},
-          wrapperName = pluralize(req.params.collectionName, 1);
       result.channel = params.channel;
-      json[wrapperName] = result;
+      var json = {tweet: result};
       res.send(json);
     });
   });
 });
 
-app.get('/:collectionName/:id', function(req, res, next) {
-  req.collection.findById(req.params.id, function(e, result) {
+app.get('/tweets/:id', function(req, res, next) {
+  req.tweets.findById(req.params.id, function(e, result) {
     if (e) return next(e);
-    var json = {},
-        wrapperName = pluralize(req.params.collectionName, 1);
-    json[wrapperName] = result;
+    var json = {tweet: result};
     res.send(json);
   });
 });
 
-app.put('/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set: req.body}, 
-                            {safe: true, multi: false}, function(e, result) {
+app.put('/tweets/:id', function(req, res, next) {
+  req.tweets.updateById(req.params.id, {$set: req.body}, 
+                        {safe: true, multi: false}, function(e, result) {
     if (e) return next(e);
     res.send((result === 1) ? {msg:'success'} : {msg: 'error'});
   });
 });
 
 /*
-app.delete('/:collectionName/:id', function(req, res, next) {
-  req.collection.removeById(req.params.id, function(e, result) {
+app.delete('/tweets/:id', function(req, res, next) {
+  req.tweets.removeById(req.params.id, function(e, result) {
     if (e) return next(e);
     res.send((result === 1) ? {msg: 'success'} : {msg: 'error'});
   });
 });
 */
 
-app.get('/:collectionName/channel/:channel', function(req, res, next) {
-  req.collection.find({channel: req.params.channel}, {limit: 30, 
+app.get('/tweets/channel/:channel', function(req, res, next) {
+  req.tweets.find({channel: req.params.channel}, {limit: 30, 
     sort: {'_id': -1}}).toArray(function(e, results) {
     if (e) return next(e);
-    var json = {},
-        wrapperName = req.params.collectionName;
-    json[wrapperName] = results;
+    var json = {tweets: results};
     res.send(json);
   });
 });
 
-app.get('/:collectionName/:id/comments', function(req, res, next) {
-  req.collection.find({in_reply_to_status_id: req.params.id}, {limit: 30, 
+app.get('/tweets/:id/comments', function(req, res, next) {
+  req.tweets.find({in_reply_to_status_id: req.params.id}, {limit: 30, 
     sort: {'_id': -1}}).toArray(function(e, results) {
     if (e) return next(e);
-    var json = {},
-        wrapperName = 'comments';
-    json[wrapperName] = results;
+    var json = {comments: results};
     res.send(json);
   });
 });
